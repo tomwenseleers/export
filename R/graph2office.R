62a81a56ce6c22268dd252322e8a9ff22de4deda
Andrew Baxter <andrewbaxter439@gmail.com>
2020-03-04 12:09:01 +0000

    fixed graph2ppt

--- R/graph2office.R
+++ R/graph2office.R
@@ -180,7 +180,7 @@ graph2office = function(x = NULL, file = "Rplot", fun = NULL, type = c("PPT","DO
     }
     if(vector.graphic){
       # doc = ph_with_vg_at(doc, code = myplot(), left = offx, top = offy, width = w, height = h, ...)
-      doc = ph_with(doc, value = dml(ggobj = temp.file, location = ph_location(left = offx, top = offy, width = w, height = h), ...))
+      doc = ph_with(doc, value = dml(ggobj = myplot()), location = ph_location(left = offx, top = offy, width = w, height = h, ...))
     } else {
       temp.file <- paste0(tempfile(), ".png")
       grDevices::png(filename = temp.file, height = h, width = w, units = "in", res = 300)
